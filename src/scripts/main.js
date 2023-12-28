ba4ecee138ac35a7539a7b7ed1b8168b1f9af44d
Dmytro Vovk <bellboy.dv@gmail.com>
2023-12-28 18:24:47 +0200

    commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,17 @@
 'use strict';
 
 const form = document.getElementById('formId');
+const page = document.getElementById('page-body');
 
 form.addEventListener('submit', (evt) => {
   evt.preventDefault();
   evt.target.reset();
+});
+
+window.addEventListener('hashchange', function() {
+  if (window.location.hash.includes('menu')) {
+    page.classList.add('page--hidden');
+  } else {
+    page.classList.remove('page--hidden');
+  }
 });
